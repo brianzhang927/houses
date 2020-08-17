258bbbd5a7f74951ca681a378c0cad20eff4855c
Brian Zhang <brianzhang927@gmail.com>
2020-08-17 15:41:37 -0400

    Update import.py

--- import.py
+++ import.py
@@ -13,14 +13,17 @@ if len(argv) != 2:
 db = SQL("sqlite:///students.db")
 
 with open(argv[1], "r") as file:
+    
     reader = DictReader(file)
 
     # Add students to database
     for row in reader:
         name = row["name"].split()
+        
         if len(name) == 3:
             db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
                        name[0], name[1], name[2], row["house"], row["birth"])
+        
         elif len(name) == 2:
             db.execute("INSERT INTO students (first, last, house, birth) VALUES(?, ?, ?, ?)",
                        name[0], name[1], row["house"], row["birth"])
